9f580aa91b42ef92abdc3dff1bf7638db6bb1b5a
Jevgeni Shibinski <jevgeni.shibinski@gmail.com>
2025-05-01 18:17:05 +0300

    Updated

---
+++ LittleLemon-iOS/Styles.swift
@@ -0,0 +1,18 @@
+//
+//  Styles.swift
+//  LittleLemon-iOS
+//
+//  Created by Eugene Sibinski on 01.05.2025.
+//
+
+import SwiftUI
+
+struct Styles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Styles()
+}
